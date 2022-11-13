858b737fa414fb365399079cc496db0d1d49977c
Luís Filipe Soutello <lfsoutello@gmail.com>
2022-11-13 17:13:55 -0300

    make example entity more simple

--- core/src/main/java/dev/lfsoutello/simpleservicetemplate/example/Example.java
+++ core/src/main/java/dev/lfsoutello/simpleservicetemplate/example/Example.java
@@ -1,16 +1,11 @@
 package dev.lfsoutello.simpleservicetemplate.example;
 
-import dev.lfsoutello.simpleservicetemplate.BaseEntity;
-import lombok.EqualsAndHashCode;
-import lombok.Setter;
-import lombok.ToString;
-
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
-@Setter
 @Entity
-@ToString
-@EqualsAndHashCode(callSuper = true)
-public class Example extends BaseEntity {
-   private String string;
+public class Example {
+   @Id
+   private Long id;
+   private String text;
 }

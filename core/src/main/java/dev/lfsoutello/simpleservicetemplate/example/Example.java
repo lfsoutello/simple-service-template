8523fc38c184418c0534d79255376faa736b2a72
Luís Filipe Soutello <lfsoutello@gmail.com>
2022-11-27 00:31:51 -0300

    spring: update to spring boot 3.0.0

--- core/src/main/java/dev/lfsoutello/simpleservicetemplate/example/Example.java
+++ core/src/main/java/dev/lfsoutello/simpleservicetemplate/example/Example.java
@@ -3,9 +3,9 @@ package dev.lfsoutello.simpleservicetemplate.example;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 
 @Setter
 @Getter

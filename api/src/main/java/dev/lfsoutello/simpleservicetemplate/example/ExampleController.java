97f10decf98f4e82003af431d786bfd5ec322ff1
Luís Filipe Soutello <lfsoutello@gmail.com>
2022-11-27 00:23:59 -0300

    feat: cotroller updates

--- api/src/main/java/dev/lfsoutello/simpleservicetemplate/example/ExampleController.java
+++ api/src/main/java/dev/lfsoutello/simpleservicetemplate/example/ExampleController.java
@@ -13,24 +13,27 @@ import org.springframework.web.bind.annotation.*;
 public class ExampleController {
     private final ExampleService exampleService;
 
-    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping
     public ResponseEntity<ExampleDTO> create(@RequestBody ExampleForm body) {
-        log.info("luis");
+        log.info("[ExampleController] m=create, body={}", body);
         return ResponseEntity.ok(exampleService.create(body.getText()));
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<ExampleDTO> read(@PathVariable Long id) {
+        log.info("[ExampleController] m=read, id={}", id);
         return ResponseEntity.ok(exampleService.read(id));
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<ExampleDTO> update(@PathVariable Long id, @RequestBody ExampleForm body) {
+        log.info("[ExampleController] m=update, id={}, body={}", id, body);
         return ResponseEntity.ok(exampleService.update(id, body.getText()));
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
+        log.info("[ExampleController] m=delete, id={}", id);
         exampleService.delete(id);
         return ResponseEntity.noContent().build();
     }
--- api/src/main/java/dev/lfsoutello/simpleservicetemplate/example/ExampleForm.java
+++ api/src/main/java/dev/lfsoutello/simpleservicetemplate/example/ExampleForm.java
@@ -2,9 +2,11 @@ package dev.lfsoutello.simpleservicetemplate.example;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Setter
 @Getter
+@ToString
 public class ExampleForm {
     private String text;
 }

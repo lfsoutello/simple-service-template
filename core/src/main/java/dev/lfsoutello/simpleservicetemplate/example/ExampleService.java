fa08af3b0612349db31b4abcabe7780e736d55f1
Luís Filipe Soutello <lfsoutello@gmail.com>
2022-11-27 01:16:45 -0300

    feat: add exception handler

--- core/src/main/java/dev/lfsoutello/simpleservicetemplate/example/ExampleService.java
+++ core/src/main/java/dev/lfsoutello/simpleservicetemplate/example/ExampleService.java
@@ -1,9 +1,8 @@
 package dev.lfsoutello.simpleservicetemplate.example;
 
+import dev.lfsoutello.simpleservicetemplate.exception.ExampleNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -19,13 +18,13 @@ public class ExampleService {
 
     public ExampleDTO read(Long id) {
         Example example = exampleRepository.findById(id)
-            .orElseThrow(() -> new RuntimeException("Example not found"));
+            .orElseThrow(ExampleNotFoundException::new);
         return new ExampleDTO(example);
     }
 
     public ExampleDTO update(Long id, String text) {
         Example example = exampleRepository.findById(id)
-            .orElseThrow(() -> new RuntimeException("Example not found"));
+            .orElseThrow(ExampleNotFoundException::new);
         example.setText(text);
         return new ExampleDTO(exampleRepository.save(example));
     }
